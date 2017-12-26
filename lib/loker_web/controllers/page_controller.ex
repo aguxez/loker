54ea6892c89cdfdc838ac48755762bf2e23d947b
aguxez <dizmiiguel@gmail.com>
2017-12-26 15:18:42 -0400

    returns correct information of request on json

--- lib/loker_web/controllers/page_controller.ex
+++ lib/loker_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule LokerWeb.PageController do
   use LokerWeb, :controller
 
   alias Loker.State.Summoners
+  alias Godfist.DataDragon
 
   def index(conn, _params) do
     csrf_token = Poison.encode!(Plug.CSRFProtection.get_csrf_token())
@@ -25,41 +26,61 @@ defmodule LokerWeb.PageController do
     {:ok, game} =
       case Summoners.get_game_for(summoner) do
         [] ->
-          IO.inspect("requested game")
           {:ok, new_game} = Godfist.active_game(server, summoner)
           Summoners.save_game_for(summoner, new_game)
 
           {:ok, new_game}
         game ->
-          IO.inspect("game found")
           {:ok, game}
       end
 
     champ_icons = get_champs_info(game)
 
-    json conn, %{game: game, champ_icons: champ_icons}
+    json(conn, %{game: game, champ_icons: champ_icons})
   end
 
   defp get_champs_info(game) do
     game["participants"]
     |> Enum.map(fn x -> x["championId"] end)
     |> Enum.reject(&(&1 == nil))
-    |> get_champs_icons()
+    |> get_champs_icons(&DataDragon.champ_square/1)
   end
 
   # DataDragon is a 3rd party so the calls don't count to the rate-limits.
-  defp get_champs_icons(champs) do
-    {:ok, champ_data} = Godfist.DataDragon.Data.champions()
+  defp get_champs_icons(champs, module_function) do
+    {:ok, champ_data} = DataDragon.Data.champions()
 
     for {_k, v} <- champ_data["data"], team_champ <- champs do
+      # 'module_function' is the DataDragon function to be used so we can reuse
+      # this function
       if String.to_integer(v["key"]) == team_champ do
-        %{v["key"] => Godfist.DataDragon.champ_square(v["name"])}
+        %{v["key"] => module_function.(v["name"])}
       end
     end
     |> Enum.reject(&(&1 == nil))
   end
 
-  def get_summoner_info(conn, %{"name" => name, "champId" => champ_id}) do
-    json conn, %{name: name, champId: champ_id}
+  def get_summoner_info(conn, %{"summId" => summId, "champId" => champ_id} = params) do
+    string_champ_id = to_string(champ_id)
+    server =
+      params["server"]
+      |> String.downcase()
+      |> String.to_atom()
+
+    # Retrieving information of a single summoner.
+    # TODO: Add this to a 'cache' too
+    %{"tier" => tier, "rank" => rank} = get_solo_league(server, summId)
+
+    # Look for a nicer way of doing this pattern match.
+    [%{^string_champ_id => load_link}] =
+      get_champs_icons([champ_id], &DataDragon.champ_loading/1)
+
+    json(conn, %{tier: tier, rank: rank, champ_loading: load_link})
+  end
+
+  defp get_solo_league(server, name) do
+    {:ok, league} = Godfist.League.positions(server, name)
+
+    Enum.find(league, &(&1["queueType"] === "RANKED_SOLO_5x5"))
   end
 end
